6379f57d28e7f896f7900f2fb0b3fce1c202e7ca
wesslingpedro <wesslingpedro@gmail.com>
2024-05-15 20:19:08 -0300

    Primeiro commit

---
+++ EncEsCond/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace testes_eec
+{
+    class Program
+    {
+        static void Main(string[] args){
+            int horas;
+            Console.WriteLine("Quantas horas? ");
+            horas = int.Parse(Console.ReadLine());
+
+            if (horas < 12) {
+                Console.WriteLine("Bom dia!");
+            }
+            else{
+              if (horas < 18){
+                    Console.WriteLine("Boa tarde!");
+
+
+            
+                }
+              else{
+                    Console.WriteLine("Boa noite!");
+                }
+            }
+            Console.ReadLine();
+        }
+         
+    }
+}
